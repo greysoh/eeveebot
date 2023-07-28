5be65fed4829e7df27943b0dccc87f7f8479c991
greysoh <dev@hofers.cloud>
2023-07-28 17:38:51 -0400

    fix: Fixes weird formatting.

--- index.mjs
+++ index.mjs
@@ -47,19 +47,19 @@ const foldersPath = join(__dirname, 'commands');
 const commandFolders = await readdir(foldersPath);
 
 for (const folder of commandFolders) {
-	const commandsPath = join(foldersPath, folder);
-	const commandFilesRaw = await readdir(commandsPath); // I love JS.
+  const commandsPath = join(foldersPath, folder);
+  const commandFilesRaw = await readdir(commandsPath); // I love JS.
   const commandFiles = commandFilesRaw.filter((file) => file.endsWith('.js') || file.endsWith('.mjs'));
-	
+  
   for (const file of commandFiles) {
-		const filePath = join(commandsPath, file);
-		const command = await import(filePath);
-		// Set a new item in the Collection with the key as the command name and the value as the exported module
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-	  } else {
-		  console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	  }
+    const filePath = join(commandsPath, file);
+    const command = await import(filePath);
+    // Set a new item in the Collection with the key as the command name and the value as the exported module
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+    }
   }
 }
 
@@ -70,24 +70,24 @@ client.once(Events.ClientReady, (c) => {
 });
 
 client.on(Events.InteractionCreate, async(interaction) => {
-	if (!interaction.isChatInputCommand()) return;
-	const command = interaction.client.commands.get(interaction.commandName);
+  if (!interaction.isChatInputCommand()) return;
+  const command = interaction.client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
 
-	try {
-		await command.execute(interaction, uidChatlogs);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
+  try {
+    await command.execute(interaction, uidChatlogs);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+    } else {
+      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+  }
 });
 
 client.on(Events.MessageCreate, async(message) => {
